1402137595c32ad2d6a03e9c5e426a49b3146cc7
Trex0319 <weichung22y@forwardschool.edu.my>
2023-10-16 12:44:35 +0800

    update

--- src/api/data.js
+++ src/api/data.js
@@ -1,1 +1,6 @@
-export const API_URL = "http://localhost:8000";
+// export const API_URL = "http://localhost:8000";
+
+export const API_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000"
+    : "http://10.1.104.4:5000";
